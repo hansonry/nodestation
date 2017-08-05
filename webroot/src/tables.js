15c569657646ba2956b1eb996ae5da1a32aee7c3
Ryan Hanson <hansonry@gmail.com>
2017-08-05 17:37:21 -0500

    Moved giant door table to a tables file

---
+++ webroot/src/tables.js
@@ -0,0 +1,169 @@
+
+var doorTypeTable = {
+   atmos: {
+      spriteSheet: "doorAtmos",
+      windowed: false,
+   },
+   atmosWindowed: {
+      spriteSheet: "doorAtmos",
+      windowed: true,
+   },
+   bananium: {
+      spriteSheet: "doorBananium",
+      windowed: false,
+   },
+   bananiumWindowed: {
+      spriteSheet: "doorBananium",
+      windowed: true,
+   },
+   command: {
+      spriteSheet: "doorCommand",
+      windowed: false,
+   },
+   commandWindowed: {
+      spriteSheet: "doorCommand",
+      windowed: true,
+   },
+   diamond: {
+      spriteSheet: "doorDiamond",
+      windowed: false,
+   },
+   diamondWindowed: {
+      spriteSheet: "doorDiamond",
+      windowed: true,
+   },
+   engineering: {
+      spriteSheet: "doorEngineering",
+      windowed: false,
+   },
+   engineeringWindowed: {
+      spriteSheet: "doorEngineering",
+      windowed: true,
+   },
+   freezer: {
+      spriteSheet: "doorFreezer",
+      windowed: false,
+   },
+   gold: {
+      spriteSheet: "doorGold",
+      windowed: false,
+   },
+   goldWindowed: {
+      spriteSheet: "doorGold",
+      windowed: true,
+   },
+   maintenance: {
+      spriteSheet: "doorMaintenance",
+      windowed: false,
+   },
+   maintenanceWindowed: {
+      spriteSheet: "doorMaintenance",
+      windowed: true,
+   },
+   maintenanceExternal: {
+      spriteSheet: "doorMaintenanceExternal",
+      windowed: false,
+   },
+   maintenanceExternalWindowed: {
+      spriteSheet: "doorMaintenanceExternal",
+      windowed: true,
+   },
+   medical: {
+      spriteSheet: "doorMedical",
+      windowed: false,
+   },
+   medicalWindowed: {
+      spriteSheet: "doorMedical",
+      windowed: true,
+   },
+   mining: {
+      spriteSheet: "doorMining",
+      windowed: false,
+   },
+   miningWindowed: {
+      spriteSheet: "doorMining",
+      windowed: true,
+   },
+   plasma: {
+      spriteSheet: "doorPlasma",
+      windowed: false,
+   },
+   plasmaWindowed: {
+      spriteSheet: "doorPlasma",
+      windowed: true,
+   },
+   public: {
+      spriteSheet: "doorPublic",
+      windowed: false,
+   },
+   publicWindowed: {
+      spriteSheet: "doorPublic",
+      windowed: true,
+   },
+   research: {
+      spriteSheet: "doorResearch",
+      windowed: false,
+   },
+   researchWindowed: {
+      spriteSheet: "doorResearch",
+      windowed: true,
+   },
+   sandstone: {
+      spriteSheet: "doorStandstone",
+      windowed: false,
+   },
+   sandstoneWindowed: {
+      spriteSheet: "doorStandstone",
+      windowed: true,
+   },
+   science: {
+      spriteSheet: "doorScience",
+      windowed: false,
+   },
+   scienceWindowed: {
+      spriteSheet: "doorScience",
+      windowed: true,
+   },
+   security: {
+      spriteSheet: "doorSecurity",
+      windowed: false,
+   },
+   securityWindowed: {
+      spriteSheet: "doorSecurity",
+      windowed: true,
+   },
+   silver: {
+      spriteSheet: "doorSilver",
+      windowed: false,
+   },
+   silverWindowed: {
+      spriteSheet: "doorSilver",
+      windowed: true,
+   },
+   uranium: {
+      spriteSheet: "doorUranium",
+      windowed: false,
+   },
+   uraniumWindowed: {
+      spriteSheet: "doorUranium",
+      windowed: true,
+   },
+   virology: {
+      spriteSheet: "doorVirology",
+      windowed: false,
+   },
+   virologyWindowed: {
+      spriteSheet: "doorVirology",
+      windowed: true,
+   },
+   wood: {
+      spriteSheet: "doorWood",
+      windowed: false,
+   },
+   woodWindowed: {
+      spriteSheet: "doorWood",
+      windowed: true,
+   },
+};
+
+
